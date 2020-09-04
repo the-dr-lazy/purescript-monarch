f2b29468becd4be411bf33a51a9a579dab9a0db6
Mohammad Hasani <thebrodmann@protonmail.com>
2020-09-05 01:38:01 +0430

    feat: change virtual DOM properties types hierarchy regard to specification

--- src/Monarch/VirtualDOM/Properties.purs
+++ src/Monarch/VirtualDOM/Properties.purs
@@ -3,15 +3,20 @@ module Monarch.VirtualDOM.Properties where
 import Monarch.VirtualDOM.Attributes    ( ClassName )
 import Type.Row                         ( type (+) )
 
-type GlobalProperties r
-  = ( className :: ClassName
+type NodeProperties r = r
+
+type ElementProperties r
+  = NodeProperties
+  + ( className :: ClassName
     | r
     )
 
-type HTMLDivProperties r = GlobalProperties r
+type HTMLElementProperties r = ElementProperties r
+
+type HTMLDivProperties r = HTMLElementProperties r
 
 type HTMLButtonProperties r
-  = GlobalProperties
+  = HTMLElementProperties
   + ( disabled :: Boolean
     | r
     )
