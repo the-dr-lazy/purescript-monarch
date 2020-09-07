1933ad81c4098039835a27210e33b374eb3aa279
Mohammad Hasani <thebrodmann@protonmail.com>
2020-09-07 20:24:04 +0430

    docs: comment OptionalRecordCons typeclasses

--- src/Monarch/Type/RowList.purs
+++ src/Monarch/Type/RowList.purs
@@ -15,9 +15,11 @@ import Type.RowList (kind RowList)
 import Type.RowList as RowList
 import Type.Row as Row
 
+-- | `RowList` version of `OptionalRecordCons` typeclass
 class OptionalRecordCons (row :: RowList) (name :: Symbol) (s :: # Type) (t :: # Type)
 
 instance nilOptionalRecordCons :: OptionalRecordCons RowList.Nil _name _s _t
+-- | Constraint target field (`name`) when it exists on given `row`
 instance consOptionalRecordCons ::
   (Row.Union t _t s) => OptionalRecordCons (RowList.Cons name { | t } tail) name s t
 else instance fallbackConsOptionalRecordCons ::
