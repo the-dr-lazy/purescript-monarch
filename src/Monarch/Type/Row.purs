22d8bdda57da5e69c83cd039a8aa2460a2112f05
Mohammad Hasani <thebrodmann@protonmail.com>
2020-09-07 20:24:04 +0430

    docs: comment OptionalRecordCons typeclasses

--- src/Monarch/Type/Row.purs
+++ src/Monarch/Type/Row.purs
@@ -3,6 +3,7 @@ module Monarch.Type.Row where
 import Type.RowList (class RowToList)
 import Monarch.Type.RowList as RowList
 
+-- | Adds an optional record constraint with type `s` for field `name` to the given `row` type
 class OptionalRecordCons (row :: # Type) (name :: Symbol) (s :: # Type) (t :: # Type)
 
 instance rowListOptionalRecordCons :: (RowToList row list, RowList.OptionalRecordCons list name s t) => OptionalRecordCons row name s t
