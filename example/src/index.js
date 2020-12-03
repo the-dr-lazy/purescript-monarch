03ecb06651753ae2baa411dae90816959a0dda56
Mohammad Hasani <thebrodmann@protonmail.com>
2020-12-03 22:55:57 +0330

    chore: change prettier configuration

--- example/src/index.js
+++ example/src/index.js
@@ -2,9 +2,9 @@ import * as Counter from '../output/Counter.Main'
 import { unsafePerformEffect } from '../output/Effect.Unsafe'
 
 function main() {
-  const element = document.getElementById('root')
+    const element = document.getElementById('root')
 
-  unsafePerformEffect(Counter.main(element))
+    unsafePerformEffect(Counter.main(element))
 }
 
 main()
