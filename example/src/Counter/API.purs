58dcca684821103c568c9c18518a4d947cca083f
Mohammad Hasani <thebrodmann@protonmail.com>
2020-12-06 01:27:24 +0330

    chore: configure headroom

--- example/src/Counter/API.purs
+++ example/src/Counter/API.purs
@@ -1,3 +1,14 @@
+{-|
+Module     : Counter.API
+Maintainer : Mohammad Hasani (the-dr-lazy.github.io) <thebrodmann@protonmail.com>
+Copyright  : (c) 2020 Monarch
+License    : MPL 2.0
+
+This Source Code Form is subject to the terms of the Mozilla Public
+License, version 2.0. If a copy of the MPL was not distributed with this
+file, You can obtain one at http://mozilla.org/MPL/2.0/.
+-}
+
 module Counter.API where
 
 import Prelude
--- src/Monarch/Web/Window.ts
+++ src/Monarch/Web/Window.ts
@@ -1,3 +1,13 @@
+/*
+ * Maintainer : Mohammad Hasani (the-dr-lazy.github.io) <thebrodmann@protonmail.com>
+ * Copyright  : (c) 2020 Monarch
+ * License    : MPL 2.0
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, version 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
 interface SetTimeout {
     (n: Int): (f: Effect<Unit>) => Effect<Int>
 }
