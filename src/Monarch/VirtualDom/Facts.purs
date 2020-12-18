6572c6526c40d48630d0e62d8622380f2b9b4abe
Mohammad Hasani <the-dr-lazy@pm.me>
2020-12-18 22:06:13 +0330

    chore: add missing headers

--- src/Monarch/VirtualDom/Facts.purs
+++ src/Monarch/VirtualDom/Facts.purs
@@ -1,3 +1,14 @@
+{-|
+Module     : Monarch.VirtualDom.Facts
+Maintainer : Mohammad Hasani (the-dr-lazy.github.io) <the-dr-lazy@pm.me>
+Copyright  : (c) 2020 Monarch
+License    : MPL 2.0
+
+This Source Code Form is subject to the terms of the Mozilla Public
+License, version 2.0. If a copy of the MPL was not distributed with this
+file, You can obtain one at http://mozilla.org/MPL/2.0/.
+-}
+
 module Monarch.VirtualDom.Facts where
 
 import Type.Row (type (+))
