5efbfc12c3a434526503a5d724bd60d06d8e6e3c
Mohammad Hasani <the-dr-lazy@pm.me>
2021-06-01 11:14:20 +0430

    chore: run Headroom

--- src/Monarch/VirtualDom/ReorderHistory.ts
+++ src/Monarch/VirtualDom/ReorderHistory.ts
@@ -1,10 +1,16 @@
+/*
+ * Maintainer : Mohammad Hasani (the-dr-lazy.github.io) <the-dr-lazy@pm.me>
+ * Copyright  : (c) 2020-2021 Monarch
+ * License    : MPL 2.0
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, version 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
 import { VirtualDomTree, DownstreamNode, realize } from 'monarch/Monarch/VirtualDom/VirtualDomTree'
 import { ChildNodeByKeyMap } from './ChildNodeByKeyMap'
 import { OutputHandlersList } from './OutputHandlersList'
-
-/**
- * ReorderHistory ADT
- */
 export type ReorderHistory<a, b> = {
     commitByKey: Map<any, ReorderHistory.Commit<a, b>>
     endInsertKeys: Set<any>
