abfba3bdab0c3c8dbc49bb3b21325afa7b5009f9
Mohammad Hasani <the-dr-lazy@pm.me>
2021-07-14 22:08:48 +0430

    chore: run Headroom

--- src/Monarch/Document.ts
+++ src/Monarch/Document.ts
@@ -1,3 +1,13 @@
+/*
+ * Maintainer : Mohammad Hasani (the-dr-lazy.github.io) <the-dr-lazy@pm.me>
+ * Copyright  : (c) 2020-2021 Monarch
+ * License    : MPL 2.0
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, version 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
 import 'setimmediate'
 
 import { OutputHandlersList } from 'monarch/Monarch/VirtualDom/OutputHandlersList'
