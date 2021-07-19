4cef24d6cbcd3cbc8c29380af442ececb4ac2e00
Mahdi Seyedan <seyedmahdiseydan78@yahoo.com>
2021-07-19 21:49:28 +0430

    chore: run headroom

--- src/Monarch/Command.purs
+++ src/Monarch/Command.purs
@@ -1,3 +1,14 @@
+{-|
+Module     : Monarch.Command
+Maintainer : Mohammad Hasani (the-dr-lazy.github.io) <the-dr-lazy@pm.me>
+Copyright  : (c) 2020-2021 Monarch
+License    : MPL 2.0
+
+This Source Code Form is subject to the terms of the Mozilla Public
+License, version 2.0. If a copy of the MPL was not distributed with this
+file, You can obtain one at http://mozilla.org/MPL/2.0/.
+-}
+
 module Monarch.Command
   ( Command
   , BASIC
