097dbfbfb3b9f2e45b12bbad7afd162f500b8ff8
Mohammad Hasani <the-dr-lazy@pm.me>
2021-07-20 09:10:34 +0430

    docs: some section comments

--- shell.nix
+++ shell.nix
@@ -3,16 +3,19 @@
 pkgs.mkShell {
   name = "purescript-monarch";
   buildInputs = with pkgs; [
+    ###################################################
     # Package managers:
     niv
     spago
 
+    ###################################################
     # Languages:
     dhall
     nodePackages.typescript
     nodejs-16_x
     purescript
 
+    ###################################################
     # LSPs:
     dhall-lsp-server
     nodePackages.purescript-language-server
@@ -21,11 +24,13 @@ pkgs.mkShell {
     nodePackages.vscode-json-languageserver-bin
     nodePackages.yaml-language-server
 
+    ###################################################
     # Code styles:
     headroom
     nixpkgs-fmt
     nodePackages.prettier
 
+    ###################################################
     # MISC:
     gitFull
     nodePackages.parcel-bundler
