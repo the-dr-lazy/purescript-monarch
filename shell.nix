e0e2aa98e4e38fba709e13ab2f1e934a93561b8c
Mohammad Hasani <the-dr-lazy@pm.me>
2021-07-20 09:06:58 +0430

    chore: add JSON and Dhall LSPs and nixpkgs-fmt

--- shell.nix
+++ shell.nix
@@ -1,18 +1,33 @@
 { pkgs ? import ./nix { } }:
 
-let
-  nodePackages = with pkgs.nodePackages; [
-    typescript
-    typescript-language-server
-    purescript-language-server
-    prettier
-    yaml-language-server
-    vscode-html-languageserver-bin
-    parcel-bundler
-  ];
-
-in pkgs.mkShell {
+pkgs.mkShell {
   name = "purescript-monarch";
-  buildInputs = with pkgs;
-    nodePackages ++ [ gitFull niv nodejs-16_x purescript spago headroom ];
+  buildInputs = with pkgs; [
+    # Package managers:
+    niv
+    spago
+
+    # Languages:
+    dhall
+    nodePackages.typescript
+    nodejs-16_x
+    purescript
+
+    # LSPs:
+    dhall-lsp-server
+    nodePackages.purescript-language-server
+    nodePackages.typescript-language-server
+    nodePackages.vscode-html-languageserver-bin
+    nodePackages.vscode-json-languageserver-bin
+    nodePackages.yaml-language-server
+
+    # Code styles:
+    headroom
+    nixpkgs-fmt
+    nodePackages.prettier
+
+    # MISC:
+    gitFull
+    nodePackages.parcel-bundler
+  ];
 }
